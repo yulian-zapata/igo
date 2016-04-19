588413ae22e07d2154adc69cd3f8ba81485f58e4
Frank0515 <frank.0515@hotmail.com>
2016-04-18 21:23:57 -0500

    Adiciones en la app

--- igo/app/src/main/java/com/igo/ucpr/igo/LoginActivity.java
+++ igo/app/src/main/java/com/igo/ucpr/igo/LoginActivity.java
@@ -63,6 +63,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
--- igo/app/src/main/java/com/igo/ucpr/igo/MainActivity.java
+++ igo/app/src/main/java/com/igo/ucpr/igo/MainActivity.java
@@ -1,19 +1,28 @@
 package com.igo.ucpr.igo;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
+
+
+
+
     Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         String fragmentTag = MainFragment.TAG;
         if (savedInstanceState == null) {
@@ -22,7 +31,6 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
             ft.add(R.id.activity_main_container, fragment, fragmentTag);
             ft.commit();
         }
-
 
     }
 

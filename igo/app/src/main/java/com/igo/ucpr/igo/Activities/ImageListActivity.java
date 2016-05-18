c6dbf327f69e9e44e39d0655f635baf9e3cd77d1
hash <hash.key66@gmail.com>
2016-05-17 21:06:29 -0500

    cargando imagenes

--- igo/app/src/main/java/com/igo/ucpr/igo/Activities/ImageListActivity.java
+++ igo/app/src/main/java/com/igo/ucpr/igo/Activities/ImageListActivity.java
@@ -2,14 +2,25 @@ package com.igo.ucpr.igo.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.google.gson.JsonElement;
+import com.igo.ucpr.igo.Networking.HttpResponse;
 import com.igo.ucpr.igo.R;
 import com.igo.ucpr.igo.Utils.LazyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ImageListActivity extends AppCompatActivity {
     ListView list;
     LazyAdapter adapter;
+    public static final List<String> Image = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +40,36 @@ public class ImageListActivity extends AppCompatActivity {
     }
 
     private String imageUrls[] = {
-            "https://si0.twimg.com/profile_images/2053165008/ac-new_bigger.jpg",
             "http://www.dromobox.com/archivos/2015/08/android-logo.jpg",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGdVJpRnJqLU5ReEk",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGb1RaZVhmUndoVXM",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGbTQ5ck45ZE52d1E",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGNWpvTm1XYnhKZEE",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGbmdIWWtSVmFCVjQ",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGSDNUaGZreERMaHM",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGSGd4eFREbVZEV00",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGdDBGQ1FtN2xTWFU",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGOWNoNzl1Rk52dTQ",
-            "https://drive.google.com/open?id=0B2ZhOfSVGhYGNVlRemFlZzBnNlE",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg"
     };
+
+
+    public static Callback<HttpResponse> callback() {
+        return new Callback<HttpResponse>() {
+
+            @Override
+            public void onResponse(Call<HttpResponse> call, Response<HttpResponse> response) {
+                if (response.isSuccessful()) {
+                    HttpResponse respuesta = response.body();
+                    CargaImagenes(respuesta.data[0]);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HttpResponse> call, Throwable t) {
+                Log.e("AdaptadorError", "" + t.getCause());
+            }
+        };
+
+    }
+
+    public static void CargaImagenes(JsonElement array) {
+        for (int i = 0; i < array.getAsJsonArray().size(); i++) {
+            JsonElement image = array.getAsJsonArray().get(i);
+            Log.e("ID ", " " + image.getAsJsonObject().get("name"));
+            Log.e("ID ", " " + image.getAsJsonObject().get("path"));
+            Image.add(i, image.getAsJsonObject().get("path").toString());
+        }
+    }
 }
